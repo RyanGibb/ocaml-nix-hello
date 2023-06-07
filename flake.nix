70995e3dddc690eaad8b66a608ce867f1ebe0e9d
Ryan Gibb <ryan@freumh.org>
2023-06-07 13:57:55 +0100

    managing dependencies

--- flake.nix
+++ flake.nix
@@ -1,23 +1,24 @@
 {
-  inputs.opam-nix.url = "github:tweag/opam-nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, opam-nix }:
+  outputs = { self, opam-nix, ... }:
     let
       system = "x86_64-linux";
-      # instantiate nixpkgs with this system
-      pkgs = nixpkgs.legacyPackages.${system};
       inherit (opam-nix.lib.${system}) buildOpamProject;
       package = "hello";
     in rec {
       packages.${system} = buildOpamProject { } package ./. {
         ocaml-base-compiler = "*";
-        ocaml-lsp-server = "*";
       };
       defaultPackage.${system} = packages.${system}.${package};
-      # create a development environment with ocaml-lsp-server
-      devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = [ defaultPackage.${system} ];
-        buildInputs = [ packages.${system}."ocaml-lsp-server" ];
-      };
     };
 }
