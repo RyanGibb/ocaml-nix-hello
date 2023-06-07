90477910ad3acb8b757bfa056c73c3a19357dba5
Ryan Gibb <ryan@freumh.org>
2023-06-07 14:16:29 +0100

    multiple systems

--- flake.nix
+++ flake.nix
@@ -1,24 +1,17 @@
 {
   inputs.opam-nix.url = "github:tweag/opam-nix";
 
-  outputs = { self, opam-nix }:
-    let
-      system = "x86_64-linux";
-      inherit (opam-nix.lib.${system}) buildOpamProject;
-      package = "hello";
-      overlay = final: prev: {
-        "${package}" = prev.${package}.overrideAttrs (_: {
-          # override derivation attributes, e.g. add additional dependacies
-          buildInputs = [ ];
-        });
-      };
-      overlayed-scope = let
-        scope = buildOpamProject { } package ./. {
+  outputs = { self, opam-nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        system = "x86_64-linux";
+        inherit (opam-nix.lib.${system}) buildOpamProject;
+        package = "hello";
+      in rec {
+        packages.${system} = buildOpamProject { } package ./. {
           ocaml-base-compiler = "*";
         };
-        in scope.overrideScope' overlay; 
-    in rec {
-      packages.${system} = overlayed-scope;
-      defaultPackage.${system} = packages.${system}.${package};
-    };
+        defaultPackage.${system} = packages.${system}.${package};
+      }
+    );
 }
