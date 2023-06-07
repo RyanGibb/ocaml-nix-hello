ce42009f3df476d6b9ee69ee56e67e08882236d5
Ryan Gibb <ryan@freumh.org>
2023-06-07 14:19:36 +0100

    combining

--- flake.nix
+++ flake.nix
@@ -1,17 +1,54 @@
 {
-  inputs.opam-nix.url = "github:tweag/opam-nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, opam-nix, flake-utils }:
+  outputs = { self, nixpkgs, opam-nix, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         system = "x86_64-linux";
-        inherit (opam-nix.lib.${system}) buildOpamProject;
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (opam-nix.lib.${system})
+          buildOpamProject
+          materializedDefsToScope
+          materializeOpamProject';
         package = "hello";
-      in rec {
-        packages.${system} = buildOpamProject { } package ./. {
+        query = {
           ocaml-base-compiler = "*";
         };
-        defaultPackage.${system} = packages.${system}.${package};
+        overlay = final: prev: {
+          "${package}" = prev.${package}.overrideAttrs (_: {
+            # override derivation attributes, e.g. add additional dependacies
+            buildInputs = [ ];
+          });
+        };
+        resolved-scope =
+          let scope = buildOpamProject { } package ./. query;
+          in scope.overrideScope' overlay;
+        materialized-scope =
+          let scope = materializedDefsToScope
+            { sourceMap.${package} = ./.; } ./package-defs.json;
+          in scope.overrideScope' overlay;
+      in rec {
+        packages = {
+          resolved = resolved-scope;
+          materialized = materialized-scope;
+          # to generate:
+          #   cat $(nix eval .#package-defs --raw) > package-defs.json
+          package-defs = materializeOpamProject' { } ./. query;
+        };
+        defaultPackage = packages.materialized.${package};
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ defaultPackage ];
+          buildInputs = [ packages."ocaml-lsp-server" ];
+        };
       }
     );
 }
